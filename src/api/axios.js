6c1e2f7c67d0e43688741c6416f4105e1e555719
Ashin kp <ashinkppyd@gmail.com>
2026-02-17 18:00:20 +0530

    jjj bb

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";  
+
 const api = axios.create({
-  baseURL: "http://3.6.89.120/api/",
+  baseURL: `${API_URL}/api/`, 
   withCredentials: true, 
 });
 
